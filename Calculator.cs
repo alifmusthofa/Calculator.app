7d1cf47484935b5724896000a890685a1ed8aaa2
alifmusthofa <muhammad.alif.musthofa@students.amikom.ac.id>
2021-03-09 15:19:03 +0700

    Melengkapi fungsi main

--- Calculator.cs
+++ Calculator.cs
@@ -2,6 +2,14 @@ Class Calculator{
 	
 	static void Main(string[] arg){
 		
+		int a = 10;
+		int b = 6;
+		
+		Console.WriteLine("Hasil Penambahan : {0} + {1} = {2}",a,b,Penambahan(a,b)); 
+		Console.WriteLine("Hasil Pengurangan: {0} + {1} = {2}",a,b,Pengurangan(a,b));
+		
+		Console.WriteLine("\nTekan Sembarang key untuk keluar");
+		Console.ReadKey();
 		
 		
 	}
